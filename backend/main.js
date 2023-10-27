287ca4db5ca3592e3601575f513c1c91eb5147a3
Akenaten <35400816+Akenaten@users.noreply.github.com>
2023-10-27 23:01:58 +0300

    User Addition Functionality implemented. Currently operational through use of a local JS object.

--- backend/main.js
+++ backend/main.js
@@ -6,118 +6,184 @@ const sqlite3 = require('sqlite3').verbose();
 
 const local_db = "./local_db/test.db"; //Database Location
 
-let test_user = {"firstName": "Jonathan" , "lastName": "Jacobo" , "email" : "yahooacc33@yahoo.com" , "password" : "totallysafe", "username" : "notAbot22"} //VALID
+let test_user = { "firstName": "Yonah",
+                  "lastName": "Abdul", 
+                  "email": "tester1234@gmail.com", 
+                  "password": "AbdulsHeaven!",
+                  "username": "DesertEagle" } ;
 
-let invalid_user = {"firstName": "Antonio" , "lastName": "Vegnini" , "email" : "colonspec2rec@hotmail.com" , "password" : "10001000"} //INVALID, NO USERNAME
 
-//CONNECT TO THE DATABASE
-let db = new sqlite3.Database(local_db, sqlite3.OPEN_READWRITE, (err)=>{
-    if (err){
+
+
+
+
+
+
+ //CONNECT TO THE DATABASE----------------------------------------------------------------------------------------
+let db = new sqlite3.Database(local_db, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
         return console.error(err.message);
     }
-    console.log("Connected to the in-memory SQLite databse.");
-    CreateUser(test_user);
+    // console.log("Connected to the in-memory SQLite databse.");
 });
+//----------------------------------------------------------------------------------------
 
 
 
-//CREATING A NEW USER
-function CreateUser(Info = null, DATABASE = "sqlite3"){
+//-------------------------------------------ADDING A NEW USER-----------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------
 
-    if(validateNewUserInfo(Info)) //IF THE INFORMATION ABOUT THE USER IS EXAMINED AND FOUND SOUND, PROCEED
-    {
+// VERIFIES THAT THE OBJECT "USER" EXISTS AND CONTAINS CERTAIN PROPERTIES / FIELDS / ATTRIBUTES.
+// PROCEEDS TO CALL checkUsrName with the "USER" object if the test is successful.
+function userInfoIntegrity(USER = null){ 
+    let current_status = true;
 
-        db.run(
-            "INSERT INTO users (username, first_name, last_name, email, password) VALUES ($user, $fname, $lname, $email, $password)" , {
-                $user: Info.username,
-                $fname: Info.firstName,
-                $lname:Info.lastName,
-                $email:Info.email,
-                $password:Info.password
-            }, err => {
-                if(err){
-                    console.log(err.message);
-                }
+    var fields = ["firstName", "lastName", "email", "username" , "password"];
+    if(USER){
+        fields.forEach(field => {
+            if(USER.hasOwnProperty(field)){
+
             }
-        )
-        
-
-    } else // IF THE INFORMATION IS BAD, DO NOTHING.
-    {
-        console.log("Unable to create user, no information provided.");
-    }
-
-}
-
-function validateNewUserInfo(Info){ //MAKE SURE USER INFORMATION IS CORRECT
-
-    if(Info) //HAS SOMETHING EVEN BEEN PROVIDED?
-    {
-        let parameters = ["firstName", "lastName", "email", "password", "username"] // PARAMETERS THE USER OBJECT MUST HAVE.
-
-        parameters.forEach(parameter => { //CHECK IF ALL OF THE ABOVE PARAMETERS EXIST, OTHERWISE HALT FURTHER EXECUTION.
-            if(!Info.hasOwnProperty(parameter)){
-                console.log(`${parameter} has failed the check.`);
-                return false;
+            else{
+                current_status = false;
+                console.log(`Field of ${field} is missing from the User object.`);
             }
         })
 
-        if(checkUsernameAvailability(Info.username)){ //IS THE USER'S CHOSEN USERNAME AVAILABLE? YES -> PROCEED / NO -> HALT EXECUTION
-            return true;
-        } else {
-            console.log("username availability failed.");
-            return false;
+        if(current_status){
+            checkUsrName(USER);
         }
+
     }
-    return false;
+    else{
+        current_status = false;
+        console.log("No user object provided.");
+    }
+
 }
 
-function checkUsernameAvailability(c_usrname){
-    db.get("SELECT * FROM users WHERE username = ?" , c_usrname, (err,row)=> {
-        if(err){
-            console.log(err.message);
-            return false;
-        } else {
-
-        }
-
-        if(row != undefined){
-            console.log("Username already exists.");
-            return false;
+//CHECKS WETHER THE user's USERNAME is already registered.
+//If not, proceeds to call checkUsrMail.
+function checkUsrName(USER){
+    db.get("SELECT username FROM users WHERE username = ?" , [USER.username] , (err, row) => {
+        if(!err){
+            if(row != undefined){
+                console.log(`Username of ${USER.username} already exists.`);
+                
+            }
+            else{
+                console.log("Username available.");
+                checkUsrMail(USER);
+            
+            }
         }
         else{
-            console.log("Username available");
-            return true;
-
+            console.log("error.");
+            
         }
     })
-
 }
 
-function checkEmailAvailability(c_email){
-    db.get("SELECT * FROM users WHERE email = ?" , c_email, (err,row)=> {
-        if(err){
-            console.log(err.message);
-            return false;
-        }
-        if(row){
-            console.log("Email already exists.");
-            return false;
+//CHECKS WETHER THE user's EMAIL is already registered.
+//If not, proceeds to call addUser.
+function checkUsrMail(USER){
+    
+    db.get("SELECT email FROM users WHERE email = ?" , [USER.email] , (err, row) => {
+        if(!err){
+            if(row != undefined){
+                console.log(`Email of ${USER.email} already exists.`);
+                
+            }
+            else{
+                console.log("Email available.");
+                addUser(USER);
+            
+            }
         }
         else{
-            console.log("Email available");
-            return true;
+            console.log("error.");
+            
+        }
+    })
+}
 
+//Adds the USER object to the "users" table of the test.db database.
+//Fields "middle_name" and "created_at" are currently optional (no need to include them in the USER object.)
+function addUser(USER){
+
+    db.run("INSERT INTO users (first_name, last_name, middle_name, username, email, password, created_at) VALUES ($fname, $lname , $mname, $usrname, $email, $pass , $date)",
+    {
+        $fname: USER.firstName,
+        $lname: USER.lastName,
+        $usrname: USER.username,
+        $email: USER.email,
+        $pass: USER.password,
+        $date: USER.created_at ? USER.created_at : '01-01-1991',
+        $mname: USER.middleName ? USER.middleName : null
+    }, (err) => {
+        if(err){
+            console.log(err.message);
+        } else {
+            console.log("User successfully added!");
         }
     });
 
 }
 
-db.close((err)=>{
+//Starting point for the addition of a new User to the test database.
+// The chain sequence goes: userInfoIntegrity -> checkUsrName -> checkUsrMail -> addUser.
+function CommenceUserAddition(USER){
+    userInfoIntegrity(USER);
+}
+
+//-----------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------------CLOSE DATABASE-----------------------
+db.close((err) => {
     if (err) {
         return console.error(err.message);
-      }
-      console.log('Close the database connection.');
+    }
+    // console.log('Close the database connection.');
 })
 
 // ----------------------------------------------------------------------------------------
