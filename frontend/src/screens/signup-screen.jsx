fc840c59a5051863bc53b3afa05dd4ee13106746
dna737 <adnanhere27@gmail.com>
2023-10-27 15:27:41 -0500

    use base form template for components

--- frontend/src/screens/signup-screen.jsx
+++ frontend/src/screens/signup-screen.jsx
@@ -1,35 +1,7 @@
-import { useState } from "react";
-import { Fragment } from "react";
+import FormScreen from "./form-screen";
 
 export default function SignupScreen() {
     const fields = ["Username", "Password", "Verify password", "Email"];
-    const [values, setValue] = useState(["", "", "", ""]);
-
-    return (
-        <div className="p-3 m-2">
-            {fields.map((field, index) => (
-                <Fragment key={field}>
-                    {fetchInputBox({ field, values, index, setValue })}
-                </Fragment>
-            ))}
-        </div>
-    );
-}
-
-function fetchInputBox({ field, values, index, setValue }) {
-    return (
-        <div>
-            <input
-                type="text"
-                placeholder={field}
-                className="input input-bordered input-info w-full max-w-xs"
-                values={values[index]}
-                onChange={(e) => {
-                    const newValues = [...values];
-                    newValues[index] = e.target.value;
-                    setValue(newValues);
-                }}
-            />
-        </div>
-    );
+    const signupPostURL = "someURLForLoginScreen";
+    return <FormScreen fields={fields} postURL={signupPostURL} />;
 }
