abfe2ea7748f333c86f6f4f797902ee13c8128ca
dna737 <adnanhere27@gmail.com>
2023-10-28 13:41:01 -0500

    use conditional rendering of options based on pathname

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,18 +1,26 @@
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export function Navbar() {
+    const pathname = useLocation().pathname;
     return (
         <nav className="p-4 items-center text-3xl absolute font-joti h-16 bg-black flex justify-between w-screen top-0 ">
-            GOLDEN HEN 🐔
+            <Link to="/">GOLDEN HEN 🐔</Link>
             <div className="mr-10 font-jost">
-                <Link to="/sign-up">
-                    <button className="mr-5 btn btn-outline btn-info">
-                        SIGN UP
-                    </button>
-                </Link>
-                <Link to="/login">
-                    <button className="btn btn-outline btn-info">LOGIN</button>
-                </Link>
+                {pathname !== "/sign-up" && (
+                    <Link to="/sign-up">
+                        <button className="btn ml-3 btn-outline btn-info">
+                            SIGN UP
+                        </button>
+                    </Link>
+                )}
+                {pathname !== "/login" && (
+                    <Link to="/login">
+                        <button className="ml-3 btn btn-outline btn-info">
+                            LOGIN
+                        </button>
+                    </Link>
+                )}
             </div>
         </nav>
     );
